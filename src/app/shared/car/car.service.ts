6f324a9252682bddc1b96695e4b73eca472a855c
Manikanta Swamyappari <manikanta.s@HCL.COM>
2018-06-14 04:01:05 +0530

    second commit with all changes

--- src/app/shared/car/car.service.ts
+++ src/app/shared/car/car.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CarService {
 
   constructor(private http: HttpClient) {
   }
 
   getAll(): Observable<any> {
-    return this.http.get(' https://kp-sbhb.herokuapp.com/cool-cars');
+    return this.http.get('https://manikanta-sbhb.herokuapp.com/cool-cars');
   }
 }
