9179d6c4f1dca0d93d7c9ec5088d5a152ab59d19
Manikanta Swamyappari <manikanta.s@HCL.COM>
2018-06-14 04:40:54 +0530

    fourth commit after adding edit features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HttpModule } from '@angular/http';
 import { CarService } from './shared/car/car.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,15 +13,35 @@ import { CarListComponent } from './car-list/car-list.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GiphyService } from './shared/giphy/giphy.service';
+import { CarEditComponent } from './car-edit/car-edit.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
     HttpClientModule,
     BrowserAnimationsModule,
